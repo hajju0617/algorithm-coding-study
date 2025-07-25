cf64b9964d75245f2717497e603044e4eb2e2edb
hajju0617 <joowon023617@gmail.com>
2025-07-25 23:16:57 +0900

    프로그래머스 Lv0 - 그림확대. Lv1 - LV2 - ----- MySQL Lv1 - Lv2 - Lv3 - Lv4 -

--- src/main/java/com/algorithm/codingtest/programmers/lv0/그림확대/Solution.java
+++ src/main/java/com/algorithm/codingtest/programmers/lv0/그림확대/Solution.java
@@ -3,23 +3,23 @@ package com.algorithm.codingtest.programmers.lv0.그림확대;
 import java.util.*;
 
 public class Solution {
-    public String[] solution(String[] picture, int k) {
-        List<String> strList = new ArrayList<>();
+        public String[] solution(String[] picture, int k) {
+            List<String> strList = new ArrayList<>();
 
-        for (String row : picture) {
-            StringBuilder Row = new StringBuilder();
-            for (char ch : row.toCharArray()) {
-                for (int i = 0; i < k; i++) {
-                    Row.append(ch);
+            for (int i = 0; i < picture.length; i++) {
+                char[] chArr = picture[i].toCharArray();
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < chArr.length; j++) {
+                    char ch = chArr[j];
+                    for (int l = 0; l < k; l++) {           // 가로 확대.
+                        sb.append(ch);
+                    }
+                }
+                String vertical = sb.toString();
+                for (int m = 0; m < k; m++) {               // 세로 확대.
+                    strList.add(vertical);
                 }
             }
-
-            String column = Row.toString();
-            for (int i = 0; i < k; i++) {
-                strList.add(column);
-            }
+            return strList.toArray(new String[0]);
         }
-
-        return strList.toArray(new String[0]);
-    }
 }

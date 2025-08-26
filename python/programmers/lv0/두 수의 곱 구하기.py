21593cd1774b0c85e4873b208cf831eb3b114a64
hajju0617 <joowon023617@gmail.com>
2025-08-26 23:26:50 +0900

    프로그래머스 (python) lv0 - 몫 구하기, 두 수의 곱 구하기, 나머지 구하기, 짝수의 합

---
+++ python/programmers/lv0/두 수의 곱 구하기.py
@@ -0,0 +1,4 @@
+def solution (num1, num2) :
+    return num1 * num2
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/120804
---
+++ python/programmers/lv0/짝수의 합.py
@@ -0,0 +1,7 @@
+def solution(n) :
+    total = 0
+    for i in range(2, n + 1, 2) :
+        total += i
+    return total
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/120831
